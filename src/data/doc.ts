9dc65c2fe49a23fe89a278de1fe074b8d202577d
Owuraku-98 <ofosuasanteo@gmail.com>
2025-06-17 16:42:33 +0100

    update manual on partnership

--- src/data/doc.ts
+++ src/data/doc.ts
@@ -76,4 +76,48 @@ export const docs: DocGroup[] = [
 			},
 		],
 	},
+	{
+		id: "name-search-reservation",
+		title: "Name Search & Reservation",
+		description:
+			"Search for the availablity of a business name. Available business name can be reserved for a period at a fee.",
+		docs: [
+			{
+				id: "name-search",
+				title: "Name Search",
+				description: "Description",
+				pageImage: null,
+			},
+			{
+				id: "name-reservation",
+				title: "Name Reservation",
+				description: "Description",
+				pageImage: {
+					url: "/sign-in-page.webp",
+					alt: "sign in img"
+				}	
+			}
+
+		]
+	},
+	{
+		id: "partnership",
+		title: "Partnership",
+		description:
+			"A partnership is made up of a minimum of two and a maximum of twenty persons who engage in profit making busniness. Their relationship is determined by a partnership agreement duly signed by all partners and stamped at the lands commission valuation division (LCVD)",
+		docs: [
+			{
+				id: "partnership-start-application",
+				title: "Start Application",
+				description: "The Start application button allows users to begin the process of registering a partnership business. Clicking the button will guide applicants through submitting the necessary information and documents required for registration, based on the partnership agreement.",
+				pageImage: null,
+			},
+			{
+				id: "partnership-registration-instruction",
+				title: "Registration Instructions",
+				description: "The Registration Instructions section provides applicants with clear, step-by-step guidance on how to successfully register a partnership business. It outlines the key processes involved, starting from conducting a name availability search to the final incorporation stage. This section also highlights the applicable processing fee, required documents, and where to access partnership information post-registration. Additionally, it reminds applicants of the mandatory annual renewal of the partnership name. By following these instructions carefully, users can ensure a smooth and compliant registration experience.",
+				pageImage: null,
+			},
+		]
+	}
 ];
