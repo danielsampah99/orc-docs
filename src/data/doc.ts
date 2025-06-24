4f7c4f91217560f8aab001f63053ee114aca1a3f
Owuraku-98 <ofosuasanteo@gmail.com>
2025-06-24 16:49:34 +0100

    feat: add corporate business name registration documentation and images

--- src/data/doc.ts
+++ src/data/doc.ts
@@ -115,5 +115,50 @@ export const docs: DocGroup[] = [
 				pageImage: null,
 			},
 		]
-	}
+	},
+	{
+		id: "public-company-limited-by-shares",
+		title: "public company limited by shares",
+		description: "A public company limited by shares is a body corporate formed without any restriction on the number of members per its constitution and its members liability is limited to the amount of unpaid shares.",
+		docs: [
+			{
+			 	id: "public-company-limited-by-shares-start-application",
+				title: "Start Application",
+				description: "The Start application button allows users to begin the process of registering a public company limited by shares. Clicking the button will guide applicants through submitting the necessary information and documents required for registration, based on the public company limited by shares agreement.",
+				pageImage: null,
+			},
+			{
+				id: "public-company-limited-by-shares-registration-instruction",
+				title: "Registration Instructions",
+				description: "The Registration Instructions section provides applicants with clear, step-by-step guidance on how to successfully register a public company limited by shares business. It outlines the key processes involved, starting from conducting a name availability search to the final incorporation stage. This section also highlights the applicable processing fee, required documents, and where to access public company limited by shares information post-registration. Additionally, it reminds applicants of the mandatory annual renewal of the public company limited by shares name. By following these instructions carefully, users can ensure a smooth and compliant registration experience.",
+				pageImage: null,
+			},
+			{
+				id: "public-company-limited-by-shares-application-form",
+				title: "Application Form",
+				description: "The Application Form section provides a comprehensive form that applicants must complete to register a public company limited by shares. This form collects essential information such as the proposed company name, registered office address, details of directors and shareholders, and other relevant data required for the registration process. It is crucial for applicants to fill out this form accurately to ensure a smooth registration experience.",
+				pageImage: null,
+			}
+		]
+	},
+	{
+		id: "corporate-business-name",
+		title: "Corporate Business Name",
+		description:
+			"A company may carry on business under a business name which does not consist of its corporate name. This is called a corporate business name . A stamp of the parent company is required and should be signed by one of its existing directors or secretary.",
+		docs: [
+			{
+				id: "corporate-business-name-instruction",
+				title: "Start Application",
+				description: "The Start application button allows users to begin the process of registering a Corporate Business Name. Clicking the button will guide applicants through submitting the necessary information and documents required for registration, based on the corporate business agreement.",
+				pageImage: null,
+			},
+			{
+				id: "corporate-business-name-registration-instruction",
+				title: "Registration Instructions",
+				description: "The Registration Instructions section provides applicants with clear, step-by-step guidance on how to successfully register a corporate business name. It outlines the key processes involved, starting from conducting a name availability search to the final incorporation stage. This section also highlights the applicable processing fee, required documents, and where to access partnership information post-registration. Additionally, it reminds applicants of the mandatory annual renewal of the partnership name. By following these instructions carefully, users can ensure a smooth and compliant registration experience.",
+				pageImage: null,
+			},
+		]
+	},
 ];
